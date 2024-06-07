fba609bf7695cfc0533289f9df746d4d5fd4d62a
Shubham <shubham170905@gmail.com>
2024-06-07 22:36:57 +0530

    Started Stat System

---
+++ Assets/Scripts/Stat/Stats.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Stat
+{
+    HP,
+    Stamina,
+    Legion,
+    Fable_Slot,
+    Guard_Regain 
+}
+
+[System.Serializable]
+public class Stats
+{
+    
+}
+
+public class StatInfo
+{
+
+}
