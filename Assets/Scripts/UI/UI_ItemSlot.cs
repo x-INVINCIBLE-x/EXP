58d8329d600deadd480ff6642a364b81b23e8bc6
Shubham <shubham170905@gmail.com>
2024-07-06 21:14:53 +0530

    Added Selection Slot UI

---
+++ Assets/Scripts/UI/UI_ItemSlot.cs
@@ -0,0 +1,55 @@
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UI_ItemSlot : MonoBehaviour, IPointerDownHandler
+{
+    [SerializeField] protected Image itemImage;
+    [SerializeField] protected TextMeshProUGUI itemText;
+    [SerializeField] protected GameObject selectionUI;
+
+    protected UI ui;
+    public InventoryItem item;
+
+    protected virtual void Start()
+    {
+        ui = GetComponentInParent<UI>();
+    }
+
+    public void UpdateSlot(InventoryItem _newItem)
+    {
+        item = _newItem;
+
+        itemImage.color = Color.white;
+
+        if (item != null)
+        {
+            itemImage.sprite = item.data.itemIcon;
+
+            if (item.stackSize > 1)
+            {
+                itemText.text = item.stackSize.ToString();
+            }
+            else
+            {
+                itemText.text = "";
+            }
+        }
+    }
+
+    public void CleanUpSlot()
+    {
+        item = null;
+
+        itemImage.sprite = null;
+        itemImage.color = Color.clear;
+        itemText.text = "";
+    }
+
+    public virtual void OnPointerDown(PointerEventData eventData)
+    { 
+        selectionUI.SetActive(true);
+    }
+
+}
